53f1359430d4a681ddc9ce2af7c788271fd7f1f4
Rodrigo Moraes <rdcmdev@gmail.com>
2017-03-17 15:50:31 -0400

    remove methods hasEdgeTo and equals from Vertex.java

--- PP1/Vertex.java
+++ PP1/Vertex.java
@@ -23,29 +23,5 @@ class Vertex {
 	
 	public void connectTo(Vertex vertex) {
 	        this.adjacencyList.add(vertex);
-	}
-	
-	public boolean equals(Object o) {
-		if(o == null){
-			return false;
-		}
-		if(o == this) {
-			return true;
-		}
-		if (!(o instanceof Vertex))return false;
-		Vertex v = (Vertex) o;
-		if(v.label == this.label) {
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean hasEdgeTo(Vertex v) {
-		for (Vertex vertex : this.getAdjacencyList()) {
-			if(vertex.equals(v)){
-				return true;
-			}
-		}
-		return false;
-	}
+	}	
 }
