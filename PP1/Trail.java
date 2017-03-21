9aff579e04749153e9f57da7b4463ff8a87670e3
Rodrigo Moraes <rdcmdev@gmail.com>
2017-03-21 12:09:47 -0400

    working

--- PP1/Trail.java
+++ PP1/Trail.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
 class Trail extends TrackAbstract {
 
@@ -19,32 +17,31 @@ class Trail extends TrackAbstract {
 	@Override
 	public boolean checkProposition(String path) {
 
-		Walk walk = new Walk(this.getGraph());		
+		Walk walk = new Walk(this.getGraph());
 		boolean isWalk = walk.checkProposition(path);
-		
+
 		if (isWalk) {
-			Set<Integer> check = new HashSet<Integer>();
+			int[][] matrix = new int[this.getGraph().getOrder()][this.getGraph().getOrder()];
 			ArrayList<Integer> vertexes = this.getVertexes(path);
-			
+
 			for (int i = 0; i < vertexes.size() - 1; i++) {
 				int a = vertexes.get(i);
-				int b = vertexes.get(i+1);
-				
-				if(a > b){
+				int b = vertexes.get(i + 1);
+
+				if (a > b) {
 					a = a + b;
 					b = a - b;
 					a = a - b;
 				}
-				
-				if(!check.add(a*1000 + b)){
-			
+
+				if ((matrix[a - 1][b - 1] += 1) >= 2) {
 					return false;
 				}
 			}
 
 			return true;
 		}
-		
+
 		return false;
 	}
 
